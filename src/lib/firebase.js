a78ac1b431e6593c10dccd9cadd211d5eca69bf9
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-05-08 13:43:56 -0400

    .

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "reactchat-b0cd1.firebaseapp.com",
+  projectId: "reactchat-b0cd1",
+  storageBucket: "reactchat-b0cd1.appspot.com",
+  messagingSenderId: "214860689483",
+  appId: "1:214860689483:web:ef4645743f8ca62c9c9dce",
+  measurementId: "G-DYPBD45P55"
 };
 
 const app = initializeApp(firebaseConfig);
